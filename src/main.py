20598ae563f38436ed523cfaa6046fa7f036929a
sofiajaves <sjaves@fi.uba.ar>
2024-09-22 15:46:32 -0300

    .

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from routers.routers import router
 from middleware.error_middleware import ErrorHandlerMiddleware
-from services.services import Services
 from utils import get_engine
 
 app = FastAPI()
