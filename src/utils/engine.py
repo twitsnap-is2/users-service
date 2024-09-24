b65889696b3debb0d99259e15243e0f1155e12e6
sofiajaves <sjaves@fi.uba.ar>
2024-09-24 15:59:29 -0300

    delete deploy.yml, tests.yml with more secrets

--- src/utils/engine.py
+++ src/utils/engine.py
@@ -5,18 +5,13 @@ import os
 from dotenv import load_dotenv
 
 def get_engine() -> Optional[create_engine]:
-    #user = os.getenv('POSTGRES_USER')
-    #password = os.getenv('POSTGRES_PASSWORD')
-    #host = os.getenv('POSTGRES_HOST')
-    #port = os.getenv('POSTGRES_PORT')
-    #db = os.getenv('POSTGRES_DB')
-    # Construcción manual de la URL
-    # database_url = f"postgresql://{user}:{password}@{host}:{port}/{db}"
-    database_url = os.getenv('DATABASE_URL')
-    if not database_url:
-        logger.error("No database URL found")
-        return ValueError("No database URL found")
-    
+    user = os.getenv('POSTGRES_USER')
+    password = os.getenv('POSTGRES_PASSWORD')
+    host = os.getenv('POSTGRES_HOST')
+    port = os.getenv('POSTGRES_PORT')
+    db = os.getenv('POSTGRES_DB')
+    database_url = f"postgresql://{user}:{password}@{host}:{port}/{db}"
+
     logger.info("Connecting Database via URL")
     
     return create_engine(database_url, echo=True)
