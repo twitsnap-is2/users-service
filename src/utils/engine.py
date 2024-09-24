9102172a9f000ebd6eae3a65af64edf6707e5bfa
sofiajaves <sjaves@fi.uba.ar>
2024-09-24 15:52:30 -0300

    change in public url secrets

--- src/utils/engine.py
+++ src/utils/engine.py
@@ -10,9 +10,8 @@ def get_engine() -> Optional[create_engine]:
     #host = os.getenv('POSTGRES_HOST')
     #port = os.getenv('POSTGRES_PORT')
     #db = os.getenv('POSTGRES_DB')
-    
     # Construcción manual de la URL
-   # database_url = f"postgresql://{user}:{password}@{host}:{port}/{db}"
+    # database_url = f"postgresql://{user}:{password}@{host}:{port}/{db}"
     database_url = os.getenv('DATABASE_URL')
     if not database_url:
         logger.error("No database URL found")
