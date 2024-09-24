bcd1d5c9ee4dbc252389f2914975216e4a9b0774
sofiajaves <sjaves@fi.uba.ar>
2024-09-24 15:47:06 -0300

    using github secrets for db url

--- src/utils/engine.py
+++ src/utils/engine.py
@@ -4,16 +4,20 @@ from loguru import logger
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-
 def get_engine() -> Optional[create_engine]:
-    user = os.getenv('POSTGRES_USER')
-    password = os.getenv('POSTGRES_PASSWORD')
-    host = os.getenv('POSTGRES_HOST')
-    port = os.getenv('POSTGRES_PORT')
-    db = os.getenv('POSTGRES_DB')
+    #user = os.getenv('POSTGRES_USER')
+    #password = os.getenv('POSTGRES_PASSWORD')
+    #host = os.getenv('POSTGRES_HOST')
+    #port = os.getenv('POSTGRES_PORT')
+    #db = os.getenv('POSTGRES_DB')
     
     # Construcción manual de la URL
-    database_url = f"postgresql://{user}:{password}@{host}:{port}/{db}"
+   # database_url = f"postgresql://{user}:{password}@{host}:{port}/{db}"
+   database_url = os.getenv('DATABASE_URL')
+   if not database_url:
+       logger.error("No database URL found")
+       return ValueError("No database URL found")
+    
+    logger.info("Connecting Database via URL")
     
     return create_engine(database_url, echo=True)
